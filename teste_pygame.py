06c86ac78b75c5ae500fac301b1fb67595fdf4cb
Chris Rhayem <rhayemm.chris@gmail.com>
2026-01-31 13:44:59 +0100

    Commentaire

--- teste_pygame.py
+++ teste_pygame.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Flash Run - Conduite lente")
 clock = pygame.time.Clock()
 
-# -------------------- DESSIN DU JOUEUR --------------------
+
 def draw_flash(position, angle):
     x, y = position
     center = (int(x + 25), int(y + 25))
@@ -31,56 +31,46 @@ def draw_flash(position, angle):
     pygame.draw.polygon(screen, (255, 255, 255), [tip, left, right])
 
 def start_game():
-    flash = [(200, 200)]     # position du joueur
-    angle = math.pi          # direction initiale
-    rotation_speed = 0.03    # rotation lente, comme une voiture
-    move_speed = 4        # déplacement rapide
+    flash = [(200, 200)]    
+    angle = math.pi          
+    rotation_speed = 0.03   
+    move_speed = 4        
 
     running = True
     while running:
-        clock.tick(60)  # 60 images par seconde
+        clock.tick(60) 
 
-        # Quitter le jeu
+       
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
 
         keys = pygame.key.get_pressed()
 
-        # Sauvegarde de l'ancienne position
         old_x, old_y = flash[0]
-
-        # Rotation lente (comme une voiture)
         if keys[K_LEFT]:
             angle -= rotation_speed
         if keys[K_RIGHT]:
             angle += rotation_speed
 
-        # Avancer / reculer
-        if keys[K_UP]:
+        if keys[K_UP]: #on avance
             flash[0] = (
                 flash[0][0] + math.cos(angle) * move_speed,
                 flash[0][1] + math.sin(angle) * move_speed
             )
-        if keys[K_DOWN]:
+        if keys[K_DOWN]: #on recule
             flash[0] = (
                 flash[0][0] - math.cos(angle) * move_speed,
                 flash[0][1] - math.sin(angle) * move_speed
             )
-
-        # Collision avec les murs (blocage)
-        if (flash[0][0] < 0 or
-            flash[0][1] < 0 or
-            flash[0][0] + 50 > 500 or
-            flash[0][1] + 50 > 500):
+        if (flash[0][0] < 0 or flash[0][1] < 0 or flash[0][0] + 50 > 500 or flash[0][1] + 50 > 500):  #si on est bloqué
             flash[0] = (old_x, old_y)
 
-        # Dessin
         screen.fill((0, 0, 0))
         draw_flash(flash[0], angle)
         pygame.display.update()
 
     pygame.quit()
 
-# -------------------- LANCEMENT --------------------
+
 start_game()
