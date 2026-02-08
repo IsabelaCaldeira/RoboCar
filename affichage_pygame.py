109a50e2a11f866bb33f4a97bdc15d1054df4bae
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-08 15:55:51 +0100

    Ajout des conditions pour la fenetre pygame

--- affichage_pygame.py
+++ affichage_pygame.py
@@ -22,9 +22,43 @@ def draw_flash(voiture):
     pygame.draw.line(screen, (255, 255, 255), center, tip, 3)
 
 def main():
+    """Cette fonction represente le main qui lance la boucle principale du programme"""
     flash = RoboCar("Flash", (200, 200), 4, 0)
 
     v_rotation= 3
     running = True
+    while running:
+        clock.tick(60)
 
-    
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+
+        keys = pygame.key.get_pressed()
+        old_x, old_y = flash.coo #on sauvegarde les anciennes coordonnees pour le cas de collision
+        if keys[K_LEFT]:
+            flash.tourner_gauche(v_rotation)
+
+        if keys[K_RIGHT]:
+            flash.tourner_droite(v_rotation)
+
+        if keys[K_UP]:
+            flash.avancer()
+
+        if keys[K_DOWN]:
+            flash.reculer()
+
+        # collision avec le mur
+        if (flash.coo[0] < 0 or flash.coo[1] < 0 or
+            flash.coo[0] + 50 > 500 or flash.coo[1] + 50 > 500):
+            flash.coo = (old_x, old_y)
+
+        screen.fill((0, 0, 0))
+        draw_flash(flash)
+        pygame.display.update()
+
+    pygame.quit()
+
+
+main()
+
