881dd4d135f834c07dd5ef793596b886fa7fdfd2
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-08 15:50:04 +0100

    Creation du main et initialisation de flash

--- affichage_pygame.py
+++ affichage_pygame.py
@@ -10,57 +10,21 @@ clock = pygame.time.Clock()
 
 
 def draw_flash(voiture):
+    """Cette fonction dessine la voiture sur l'écran"""
     x, y = voiture.coo
     center = (int(x + 25), int(y + 25))
 
     pygame.draw.circle(screen, (34, 139, 34), center, 25)
 
     angle_rad = math.radians(voiture.a)
-    tip = (
-        center[0] + math.cos(angle_rad) * 15,
-        center[1] + math.sin(angle_rad) * 15
-    )
+    tip = (center[0] + math.cos(angle_rad) * 15,center[1] + math.sin(angle_rad) * 15)
 
     pygame.draw.line(screen, (255, 255, 255), center, tip, 3)
 
-
-def start_game():
+def main():
     flash = RoboCar("Flash", (200, 200), 4, 0)
 
-    rotation_speed = 3
+    v_rotation= 3
     running = True
 
-    while running:
-        clock.tick(60)
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-
-        keys = pygame.key.get_pressed()
-        old_x, old_y = flash.coo
-        if keys[K_LEFT]:
-            flash.tourner_gauche(rotation_speed)
-
-        if keys[K_RIGHT]:
-            flash.tourner_droite(rotation_speed)
-
-        if keys[K_UP]:
-            flash.avancer(flash.v)
-
-        if keys[K_DOWN]:
-            flash.reculer(flash.v)
-
-        # collision avec les bords (inchangé)
-        if (flash.coo[0] < 0 or flash.coo[1] < 0 or
-            flash.coo[0] + 50 > 500 or flash.coo[1] + 50 > 500):
-            flash.coo = (old_x, old_y)
-
-        screen.fill((0, 0, 0))
-        draw_flash(flash)
-        pygame.display.update()
-
-    pygame.quit()
-
-
-start_game()
+    
