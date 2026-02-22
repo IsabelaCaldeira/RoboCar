c560a305bff340cb90db5cadb4908ab342d03cdf
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-22 15:45:44 +0100

    ajout de la vitesse des rouges ainsi que la longueur et largeur du robot

--- robocar.py
+++ robocar.py
@@ -1,60 +1,16 @@
 import math
-import obstacle
 
-class RoboCar(object):
-    def __init__(self, nom:str, coordonnees:tuple, vitesse:int, angle:int, rayon:int):
-        self.n = nom    # nom:string
-        self.coo = coordonnees  # coordonnees:tuple(int, int)
-        self.v = vitesse    # vitesse:int
-        self.a = angle # angle:int [0;360]
-        self.r = rayon # rayon(int)
-        
-    def avancer(self):
-        """Avance la voiture selon son angle"""
-        angle_rad = math.radians(self.a) 
-        x = self.coo[0] + math.cos(angle_rad) * self.v
-        y = self.coo[1] + math.sin(angle_rad) * self.v
-        self.coo = (x, y)
-        
-    def reculer(self):
-        """Recule la voiture selon son angle"""
-        angle_rad = math.radians(self.a) #on met en radians pour que cos et sin puissent marcher 
-        x = self.coo[0] - math.cos(angle_rad) * self.v
-        y = self.coo[1] - math.sin(angle_rad) * self.v
-        self.coo = (x, y)
-        
-    def tourner_gauche(self, vitesse):
-        self.a = (self.a - vitesse) % 360 #on veut que l'angle reste entre 0 et 360 donc on fait un modulo 360
 
-    def tourner_droite(self, vitesse):
-        self.a = (self.a + vitesse) % 360
+class RoboCar:
+    WHEEL_BASE = 50  # distance entre roues 
 
-    def mur_collision(self, largeur, hauteur, ancienne_pos):
-        x, y = self.coo
-        if x < 0 or y < 0 or x + 2 * self.r > largeur or y + 2 * self.r > hauteur:
-            self.coo = ancienne_pos
+    def __init__(self, nom, coordonnees, angle):
+        self.nom = nom
+        self.x, self.y = coordonnees #coordonne du centre du robot
+        self.angle = math.radians(angle) #orientation
 
-    def collision(self, obstacle):
-        """Cette fonction verifie si la voiture entre en collision avec un obstacle"""
-        x1, y1 = self.coo #coordonnées du robot
-        l1 = h1 = 2 * self.r   # taille du robot
-
-        x2, y2 = obstacle.pos #coordonnées de l'obstacle
-        l2, h2 = obstacle.dim #taille de l'obstacle
-        return (
-            x1 < x2 + l2 and #Le bord gauche du robot est avant le bord droit de l’obstacle
-            x1 + l1 > x2 and #le bord droit du robot est après le bord gauche de l’obstacle
-            y1 < y2 + h2 and #Le haut du robot est au-dessus du bas de l’obstacle
-            y1 + h1 > y2 #et le bas du robot est en dessous du haut de l’obstacle
-        )
-    
-    def contourne(self, coord, angle, obstacles):
-        "cette fonction fait tourner la voiture jusqu'à ce qu'elle puisse avancer sans collision"
-        self.coo = coord
-        self.a = angle
-        for obs in obstacles:
-            if self.collision(obs):
-                self.tourner_droite(90)
-                self.avancer()
-                return
-    
+        # vitesses roues
+        self.v_l = 0 #vitesse roue gauche
+        self.v_r = 0 #vitesse roue droite
+        self.largeur = 40   # largeur (cote roues)
+        self.longueur = 60  # longueur (avant/arriere)
