fa03e1227b6ad437387da36a57ec14e95e18623d
AC.ME <cyril.merlin2@gmail.com>
2026-03-23 14:27:02 +0100

    Ajout de la classe polygone (defini par x points de type Vecteur)

--- roboCar/Source/Model/obstacle.py
+++ roboCar/Source/Model/obstacle.py
@@ -1,5 +1,5 @@
 import random
-
+from Model.vecteur import Vecteur
 
 class Obstacle(object):
     def __init__(self, forme: str, position: tuple, dimensions: tuple):
@@ -11,3 +11,12 @@ class Obstacle(object):
         y = self.pos[1] + random.randint(0,500)
         self.pos = (x, y)
         return self.pos
+
+class Polygone:
+    def __init__(self, points:list[Vecteur]):
+        """ Initialisation du polygone, défini par une liste de points (à definir dans l'ordre du perimètre) """
+        self.points = points
+    
+    def get_points(self):
+        """ Renvoie les points du polygone dans l'ordre """
+        return self.points
