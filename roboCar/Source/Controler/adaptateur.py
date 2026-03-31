8e4e47ca0376865f3ac4abd1dd9431b467a9c8f7
Chris Rhayem <rhayemm.chris@gmail.com>
2026-03-31 16:43:51 +0200

    suppression de tourner_gauche tourner_droite de adapatateur.py car ils sont inutiles

--- roboCar/Source/Controler/adaptateur.py
+++ roboCar/Source/Controler/adaptateur.py
@@ -2,66 +2,46 @@ from abc import ABC, abstractmethod
 
 
 class Adaptateur(ABC):
+    """Classe abstraite commune aux adaptateurs
+    """
+
     @abstractmethod
-    def initialise(self):
+    def calculer_vitesse(self, v, w):
+        """Convertit une vitesse lineaire v et une vitesse angulaire w
+        en vitesse de roue gauche et de roue droite"""
         pass
 
     @abstractmethod
-    def get_position(self):
-        pass
-
-    @abstractmethod
-    def get_angle(self):
-        pass
-
-    @abstractmethod
-    def get_wheel_speeds(self):
-        pass
-
-    @abstractmethod
-    def set_vitesse_gauche(self, vitesse):
-        pass
-
-    @abstractmethod
-    def set_vitesse_droite(self, vitesse):
-        pass
-
-    @abstractmethod
-    def avancer(self, vitesse):
-        pass
-
-    @abstractmethod
-    def reculer(self, vitesse):
-        pass
-
-    @abstractmethod
-    def arreter(self):
-        pass
-
-    @abstractmethod
-    def tourner_sur_place(self, vitesse):
-        pass
-
-    @abstractmethod
-    def tourner_gauche(self, vitesse):
-        pass
-
-    @abstractmethod
-    def tourner_droite(self, vitesse):
-        pass
-
-    @abstractmethod
-    def get_motor_position(self):
+    def get_distance(self):
+        """Retourne la distance jusqu'a l'obstacle le plus proche"""
         pass
 
     @abstractmethod
     def get_distance_parcourue(self):
+        """Retourne la distance parcourue depuis le dernier appel"""
         pass
 
     @abstractmethod
     def get_angle_parcouru(self):
+        """ Retourne l'angle parcouru depuis le dernier appel"""
         pass
 
     @abstractmethod
-    def stop(self):
+    def avancer(self, vitesse):
+        """Fait avancer le robot"""
+        pass
+
+    @abstractmethod
+    def reculer(self, vitesse):
+        """Fait reculer le robot"""
+        pass
+
+    @abstractmethod
+    def arreter(self):
+        """Arrete le robot"""
+        pass
+
+    @abstractmethod
+    def tourner_sur_place(self, vitesse):
+        """Fait tourner le robot sur lui-meme"""
         pass
