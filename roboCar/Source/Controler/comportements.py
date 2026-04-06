8d31e9a05fd21a635670ded986e59d026ce82598
Chris Rhayem <rhayemm.chris@gmail.com>
2026-04-06 12:43:29 +0200

    creation de comportements.py qui contient la partie qui cree la startegie presents avant dans main.py

---
+++ roboCar/Source/Controler/comportements.py
@@ -0,0 +1,25 @@
+from .strategies import AvancerXMetres, TournerXDegrees, Sequence, Condition, Boucle
+def obstacle_proche(adaptateur):
+    """Retourne True si un obstacle est detecte a une distance proche"""
+    return adaptateur.get_distance() < 60
+
+
+def creer_strategie(adaptateur):
+    """Cree la strategie globale du robot"""
+    #phase de depart
+    depart = Sequence([
+        AvancerXMetres(adaptateur, 30, 2),
+    ])
+    #comportement reactif,si obs proche on tourne et on avance un peu sinon
+    reaction = Condition(
+        TournerXDegrees(adaptateur, 45, 0.08, 2),
+        AvancerXMetres(adaptateur, 2, 2),
+        adaptateur,
+        obstacle_proche
+    )
+    #strategie globale
+    strat = Sequence([
+        depart,
+        Boucle(reaction)
+    ])
+    return strat
