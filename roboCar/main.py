a8c1ae2b2708a302dc190f2c84ce9c5a779a371d
Rim <rimbroush@gmail.com>
2026-04-27 13:43:46 +0200

    lodif de main en fonctions des nouveaux changements

--- roboCar/main.py
+++ roboCar/main.py
@@ -9,26 +9,40 @@ HAUTEUR = 600
 
 def main():
     robot = RoboCar("Flash", (400, 300), 0) #creation du robot
-    zone_robot = (
-        robot.x - robot.longueur / 2,
-        robot.y - robot.largeur / 2,
-        robot.longueur,
-        robot.largeur,
+    zone_carre = (
+        robot.x - 40,
+        robot.y - 40,
+        200,
+        200,
     )
-    sim = Simulation(LARGEUR, HAUTEUR, zone_interdite=zone_robot) #creation du monde
+    sim = Simulation(LARGEUR, HAUTEUR, zone_interdite=zone_carre) #creation du monde
     robot.simulation = sim
     adp = AdaptateurSimule(robot) #adaptateur de pilotage
     view = Affichage(LARGEUR, HAUTEUR) #affichage
-    strat = creer_strategie(adp) #creation de la strategie globale
+    strat = creer_strategie(adp, sim) #creation de la strategie globale
     strat.start()
+    phase_3_preparee = False
+    position_phase_3 = (LARGEUR / 2, HAUTEUR / 2)
     running = True
     while running:
+        if strat.i >= 2 and not phase_3_preparee:
+            sim.obstacles = []
+            robot.appliquer(position_phase_3[0], position_phase_3[1], 0)
+            adp.arreter()
+            adp.synchroniser()
+            sim.initialiser_souris(vitesse=1)
+            phase_3_preparee = True
+
+        if strat.i >= 2:
+            sim.deplacer_souris()
+
         strat.step() #execution d'un pas de strategie
         #mise a jour physique du robot
         if not robot.step():
             adp.arreter()
         #affichage
-        running = view.update(robot, sim.obstacles)
+        souris = sim.souris if strat.i >= 2 else None
+        running = view.update(robot, sim.obstacles, souris=souris)
 
         time.sleep(0.01)
 
